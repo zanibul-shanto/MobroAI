02dba9c0abc13ca4d0694af52f2f833760e82063
shanto <zanibul.shanto@gmail.com>
2026-04-29 23:33:48 +0600

    removed mobile app

--- apps/api/MorboMobile/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MorboMobile;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
